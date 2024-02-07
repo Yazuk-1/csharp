e6c51194581730ab7b84b40cbaa25f7c6e7c22bf
Yazdan <117294950+Yazuk-1@users.noreply.github.com>
2024-02-07 16:06:10 +0500

    Add files via upload

---
+++ classtts/classtts/Program.cs
@@ -0,0 +1,189 @@
+﻿//int number = 3;
+//int number2 = 4;
+
+//Console.WriteLine(number+number2);
+
+
+//string name = "manahil";
+//Console.WriteLine(name);
+
+//name = "qadir";
+//Console.WriteLine("hello " + name);
+
+//int a=5, b=6 , c=7 , d=8 ;
+//Console.WriteLine(a);
+//Console.WriteLine(b);
+//Console.WriteLine(c);
+//Console.WriteLine(d);
+
+
+//int numb = 5;
+//double myDouble = 5.9;
+//char mychar = 'f';
+//bool mybool = false;
+//string mystring = "hello guys";
+//long mylong = 155655555;
+
+//int manahil = 5;
+//float myfloat = manahil;
+//double mydouble = myfloat;
+//Console.WriteLine(manahil.GetType());
+//Console.WriteLine(myfloat.GetType());
+//Console.WriteLine(mydouble.GetType());
+
+
+
+//Console.WriteLine("Naam Batao Jani " );
+//string username = Console.ReadLine();
+
+//Console.WriteLine("Han bhai " + username + "\a kese ho" );
+
+
+//Arithematic operators
+
+
+//int sum1 = 99999 * 10000;
+// Console.WriteLine(sum1);
+
+//int x = 5;
+//Console.WriteLine(x);
+
+//x >>= 2;
+//Console.WriteLine(x);
+
+//double mydouble = 9.99;
+//int myint =(int) (mydouble);
+//Console.WriteLine(myint);
+
+
+
+
+
+
+//int myint2 = 5;
+//double mydouble2 = (double) (myint2);
+//Console.WriteLine(mydouble2.GetType());
+
+
+//int a = 4;
+//int b = 5;
+
+//Console.WriteLine(a);
+
+// int i = a++;
+
+//Console.WriteLine(i);
+
+//Console.WriteLine(a);
+
+
+//assignment operator
+//int a = 5;
+
+//a -= 2;
+
+//Console.WriteLine(a);  
+
+
+//comparision operator
+//int x = 5;
+//int y = 6;
+//Console.WriteLine(y == x);
+
+//string a = "manahil";
+//string hoshiyar = "manahil";
+//Console.WriteLine(a == hoshiyar);
+
+
+//int x = 5;
+//Console.WriteLine(!(x > 3 && x==10));
+
+//int x = 6;
+//int y = 8;
+
+//Console.WriteLine(Math.Min(x,y));
+
+//string greet = "haloooo";
+//string world = "warald";
+//string peepsi = greet +  " "+world;
+//Console.WriteLine(peepsi);
+
+
+//string name = "Hira";
+//string lastname = "Rashid";
+
+//string fullname =$"{name} {lastname}";
+
+//Console.WriteLine(fullname);
+
+//string greet = "hello";
+//Console.WriteLine(greet[3] +" "+greet[1]);
+
+//Console.WriteLine("hira and manahil are women");
+
+
+int x = 10;
+int y = 15;
+//Console.WriteLine(x>y);
+
+//if(x>y)
+//{
+//    Console.WriteLine("\'x\' is greater");
+//}
+//else
+//{
+//    Console.WriteLine("\'y\' is greater");
+//}
+
+
+
+
+
+//string c = (x < y) ? "yes is is" : "not it is not";
+
+//Console.WriteLine(c);
+
+//string std = "three";
+
+//switch(std)
+//{
+//    case "one":
+//        Console.WriteLine("five students are there");
+//        break;
+//    case "2":
+//        Console.WriteLine("four students are there");
+//        break ;
+//    case "threee":
+//        Console.WriteLine("three students are there");
+//        break;
+//}
+
+
+//while 7022024
+//int i = 5;
+//while (i < 5)
+//{
+//    Console.WriteLine(i);
+//    i++;
+//}
+
+//dowhile 7022024
+//do
+//{
+//    Console.WriteLine(i);
+//    i++;
+//}
+//while (i < 5);
+
+// for loop 07022024
+
+//for (int i = 0; i <= 5; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+string[] cars = { "Volvo", "toyota", "Ford", "Mazda" };
+foreach (string i in cars)
+{
+    Console.WriteLine(i);
+}
